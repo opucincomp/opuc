573cd870b9c50fd7827e61054f11f73b1633a84f
opucincomp <samadislam187@gmail.com>
2023-07-06 11:32:09 +0530

    import changes

--- dsl/src/main.groovy
+++ dsl/src/main.groovy
@@ -3,12 +3,12 @@ def job2 = readFileFromWorkspace("${WORKSPACE}/dsl/src/job2.groovy")
 
 
 job('job1') {
-      configure(job1)
+      job1
 }
 
 
 job('job2') {
-      configure(job2)
+      job2
 }
 
 // load("${WORKSPACE}/dsl/src/job1.groovy")
