a9af6fdb6422af67384f01eb74ac90595e13de41
opucincomp <samadislam187@gmail.com>
2023-07-06 11:56:21 +0530

    pipeline added

---
+++ dsl/src/job3.groovy
@@ -0,0 +1,13 @@
+definition {
+    cpsScm {
+        scm {
+            git {
+                remote {
+                    url('https://github.com/jenkinsci/job-dsl-plugin.git')
+                }
+            branch('*/master')
+            }
+        }
+        lightweight()
+    }
+}
--- dsl/src/main.groovy
+++ dsl/src/main.groovy
@@ -1,6 +1,6 @@
 def job1 = readFileFromWorkspace("${WORKSPACE}/dsl/src/job1.groovy")
 def job2 = readFileFromWorkspace("${WORKSPACE}/dsl/src/job2.groovy")
-
+def job3 = readFileFromWorkspace("${WORKSPACE}/dsl/src/job3.groovy")
 
 job('job1') {
       job1
@@ -11,5 +11,8 @@ job('job2') {
       job2
 }
 
+pipelineJob('job3'){
+    job3
+}
 // load("${WORKSPACE}/dsl/src/job1.groovy")
 // load("${WORKSPACE}/dsl/src/job2.groovy")
